19078d400e667450eefb59eba759d255f18de9ab
Mukul Kumar <mukul0000kumar@gmail.com>
2022-01-06 23:54:59 +0530

    Modify string_operations.go 1

--- string_operations.go
+++ string_operations.go
@@ -2,11 +2,24 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
-	fmt.Println("helloworl")
-
+	a := "helloworld hellouniverse"
+	p := "   " + a + "   "
 	// trim a string
+	b := strings.Trim(a, "he")
+	c := strings.TrimLeft(a, "hel")
+	d := strings.TrimRight(a, "rse")
+	e := strings.TrimSuffix(a, "verse")
+	f := strings.TrimPrefix(a, "hel")
+	s := strings.TrimSpace(p)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+	fmt.Println(e)
+	fmt.Println(f)
+	fmt.Println(s)
 
 }
