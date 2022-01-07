c41ef182f3414bc6765505b28cb10b52d5608179
Mukul Kumar <mukul0000kumar@gmail.com>
2022-01-07 23:36:41 +0530

    Modify arrays.go 2

--- arrays.go
+++ arrays.go
@@ -16,6 +16,17 @@ func main() {
 	b[0] = "I"
 	b[1] = "Love"
 	b[2] = "Sonali"
+	// I was happy after meeting you today so wanted to tell someone but didn't find anyone whom I can tell what I felt so writing in this code
+	// So if you are reading this ignore it
+	// Just expressing my inner feelings to my code
 	fmt.Println(b)
+	// we can also assign values at the time of creating vaariables
+	var c [3]string = [3]string{"a", "b", "c"}
+	fmt.Println(c)
+	// short hand declaration for array
+	d := [5]int{1, 2, 3, 4, 5}
+	e := [5]int{}
+	fmt.Println(d)
+	fmt.Println(e)
 
 }
