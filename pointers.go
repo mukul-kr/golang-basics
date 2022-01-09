4f00a6ff468a91c71d3931cf3e95c3998baf4ce9
Mukul Kumar <mukul0000kumar@gmail.com>
2022-01-09 23:16:42 +0530

    Modify pointers.go 3

--- pointers.go
+++ pointers.go
@@ -2,11 +2,15 @@ package main
 
 import "fmt"
 
+func goodfunction(ptr *int) {
+	*ptr = *ptr + 143
+}
+
 func main() {
 	// Creating a pointer
 	var a = 5
 	var ptra *int = &a // creating a pointer
-	fmt.Println(*ptra)
+	fmt.Println(*ptra) // dereferencing a pointer to get value stored in that memory location
 
 	// short hand declaration
 	b := 10
@@ -16,4 +20,9 @@ func main() {
 	var ptrn *int
 	fmt.Println(ptrn) // while dereferencing this nill pointer will generate error
 
+	goodfunction(ptra)
+	fmt.Println(a)
+
+	goodfunction(&b)
+	fmt.Println(b)
 }
