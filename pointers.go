35228ebe4b0244ee4dc871f241656db3597d927a
Mukul Kumar <mukul0000kumar@gmail.com>
2022-01-09 23:27:37 +0530

    Modify Pointers.go 5

--- pointers.go
+++ pointers.go
@@ -25,14 +25,20 @@ func main() {
 	var ptrn *int
 	fmt.Println(ptrn) // while dereferencing this nill pointer will generate error
 
+	// passing address to function through pointers
 	goodfunction(ptra)
 	fmt.Println(a)
 
 	goodfunction(&b)
 	fmt.Println(b)
 
+	// pointer to a struct
 	emp := Employee{"Mukul", 143}
 	ptrs := &emp
 	fmt.Println((*ptrs).name)
 
+	d := "doyouloveme-givemeahint-pls"
+	ptrd := &d
+	p_ptrd := &ptrd
+	fmt.Println(**p_ptrd)
 }
