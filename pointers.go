af8453df92a03c0a3881bfdc462a9e303374e0d6
Mukul Kumar <mukul0000kumar@gmail.com>
2022-01-09 23:12:28 +0530

    Modify pointers.go 2

--- pointers.go
+++ pointers.go
@@ -3,8 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	// fmt.Println("helloworld")
-	a := 5
-	ptra := &a
+	// Creating a pointer
+	var a = 5
+	var ptra *int = &a // creating a pointer
 	fmt.Println(*ptra)
+
+	// short hand declaration
+	b := 10
+	ptrb := &b
+	fmt.Println(*ptrb)
+
+	var ptrn *int
+	fmt.Println(ptrn) // while dereferencing this nill pointer will generate error
+
 }
