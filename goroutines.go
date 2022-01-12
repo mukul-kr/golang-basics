97879f840e6e5e0c3017998ba00e09d077a3b0d3
Mukul Kumar <mukul0000kumar@gmail.com>
2022-01-12 23:41:45 +0530

    Modify goroutines.go 2

--- goroutines.go
+++ goroutines.go
@@ -11,6 +11,14 @@ func display(str string) {
 	}
 }
 
+func fun1(channel1 chan string) {
+	time.Sleep(1 * time.Second)
+	channel1 <- "Hi 🙂"
+}
+func fun2(channel2 chan string) {
+	time.Sleep(2 * time.Second)
+	channel2 <- "hello 🤣"
+}
 func main() {
 	go display("hi")
 	display("bye")
@@ -18,4 +26,18 @@ func main() {
 	// go display("hi")
 	time.Sleep(1 * time.Second)
 	display("bye")
+
+	ch1 := make(chan string)
+	ch2 := make(chan string)
+
+	go fun1(ch1)
+	go fun2(ch2)
+
+	select {
+	case option1 := <-ch1:
+		fmt.Println(option1)
+	case option2 := <-ch2:
+		fmt.Println(option2)
+	}
+
 }
