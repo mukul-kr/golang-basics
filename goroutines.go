3385be4b6d62a59e1a955a5b004023f0fe640b0e
Mukul Kumar <mukul0000kumar@gmail.com>
2022-01-12 23:52:21 +0530

    Modify goroutines.go 3

--- goroutines.go
+++ goroutines.go
@@ -20,22 +20,26 @@ func fun2(channel2 chan string) {
 	channel2 <- "hello 🤣"
 }
 func main() {
-	go display("hi")
+
+	// Normal display of goroutine
+	go display("hi") // this one needs a little time to be called since it is a go routine so a goroutine has to be created so later function is run before it as its o/p is stored in buffer
+	display("bye")   // this little no time to be called as it is normal function
+
+	time.Sleep(1 * time.Second) // we need a little time to sleep because we have to prine what is in buffer as both above function were called at almost same time so one got printed while other is waiting to be printed. If below line is executed at exactly same time print buffer will be cleared and thus above function called by goroutine will not print its result as buffer was overwritten..
 	display("bye")
 
-	// go display("hi")
-	time.Sleep(1 * time.Second)
-	display("bye")
-
+	// making channel through which information can be transferred
 	ch1 := make(chan string)
 	ch2 := make(chan string)
 
+	// both goroutines started in seperate thread provided by go
 	go fun1(ch1)
 	go fun2(ch2)
 
+	// select returns(runs) whichever gives outpus faster
 	select {
 	case option1 := <-ch1:
-		fmt.Println(option1)
+		fmt.Println(option1) //this one gives output faster here as this one sleeps for about one second only so gives output through its channel faster and thus it this case runs...
 	case option2 := <-ch2:
 		fmt.Println(option2)
 	}
