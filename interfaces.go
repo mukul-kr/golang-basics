c410d30da4c3995284e92fc935f8ae53bb67f433
Mukul Kumar <mukul0000kumar@gmail.com>
2022-01-13 23:59:37 +0530

    Modify interfaces.go 2

--- interfaces.go
+++ interfaces.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+// creating a type of interface which will calulate all passed function at time of passing it like later we can se that after creating interface we created struct over it so all function were calculated at that time only which is a good thing
 type interfeace_name interface {
 	add() int
 	subtraction() int
@@ -9,11 +10,13 @@ type interfeace_name interface {
 	division() int
 }
 
+// create a basic struct which has two sub types as int
 type two_num struct {
 	num1 int
 	num2 int
 }
 
+// 4 function which do addition sub mul div
 func (t two_num) add() int {
 	return t.num1 + t.num2
 }
